1292252dc2403a6fbdaaf2ee7d1fe7b58e989718
Abhay Raj Gupta <111484446+abhayrajgupta128@users.noreply.github.com>
2024-09-05 19:29:45 +0530

    vercel2

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -29,7 +29,7 @@ const LoginPage = () => {
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
         <h1 className="text-4xl text-center mb-4">Login</h1>
-        <form className="max-w-md mx-auto " onSubmit={handleLoginSubmit}>
+        <form className="max-w-md mx-auto " method="POST" onSubmit={handleLoginSubmit}>
           <input
             type="email"
             placeholder="your@email.com"
